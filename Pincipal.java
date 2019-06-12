ea3affcb20800cc1979d1332d95602a794e95c4c
svargas-sys <51766047+svargas-sys@users.noreply.github.com>
2019-06-12 19:43:02 -0400

    Update Pincipal.java

--- Pincipal.java
+++ Pincipal.java
@@ -3,7 +3,7 @@ public class Principal{
         public static void main (String[] args){
            
                 System.out.println("Hola mundo");
-				System.out.println("Otra línea");
+	        System.out.println("Otra línea");
 
         }
 
